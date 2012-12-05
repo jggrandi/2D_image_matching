5a2adfa5c002a54feb7f91af1d222e85d4e8c68e
Jeronimo Grandi <jggrandi@inf.ufrgs.br>
2012-12-05 17:17:31 -0200

    Carrega 3 imagens, compara as 2 ultimas com a primeira e diz qual eh a mais parecida

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 
 
 
-#include <iostream>	// for standard I/O
-#include <string>   // for strings
-#include <iomanip>  // for controlling float print precision
-#include <sstream>  // string to number conversion
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <sstream>
 
 #include <opencv2/imgproc/imgproc.hpp>  // Gaussian Blur
 #include <opencv2/core/core.hpp>        // Basic OpenCV structures (cv::Mat, Scalar)
@@ -22,18 +22,23 @@ int main(int argc, char *argv[])
 
     Mat img1;
     Mat img2;
+    Mat img3;
 
     img1 = imread( argv[1], -1 );
     img2 = imread( argv[2], -1 );
+    img3 = imread( argv[3], -1 );
 
-    const char* WIN_UT = "Under Test";
+    const char* WIN_UT1 = "Under Test1";
+    const char* WIN_UT2 = "Under Test2";
     const char* WIN_RF = "Reference";
 
     // Windows
     namedWindow(WIN_RF, CV_WINDOW_AUTOSIZE );
-    namedWindow(WIN_UT, CV_WINDOW_AUTOSIZE );
-    cvMoveWindow(WIN_RF, 400       ,            0);		 //750,  2 (bernat =0)
-    cvMoveWindow(WIN_UT, 400,            0);		 //1500, 2
+    namedWindow(WIN_UT1, CV_WINDOW_AUTOSIZE );
+    namedWindow(WIN_UT2, CV_WINDOW_AUTOSIZE );
+    cvMoveWindow(WIN_RF, 10       ,            0);		 //750,  2 (bernat =0)
+    cvMoveWindow(WIN_UT1, 522,            0);		 //1500, 2
+    cvMoveWindow(WIN_UT2, 1024,            0);		 //1500, 2
 
     double psnrV;
     Scalar mssimV;
@@ -52,15 +57,36 @@ int main(int argc, char *argv[])
 			<< " G " << setiosflags(ios::fixed) << setprecision(2) << mssimV.val[1] * 100 << "%"
 			<< " B " << setiosflags(ios::fixed) << setprecision(2) << mssimV.val[0] * 100 << "%";
 	//}
+	int m1=mssimV.val[2];
+	cout << endl;
+
+	psnrV = getPSNR(img1,img3);					//get PSNR
+	cout << setiosflags(ios::fixed) << setprecision(3) << psnrV << "dB";
+
+	//////////////////////////////////// MSSIM /////////////////////////////////////////////////
+	//if (psnrV < psnrTriggerValue && psnrV)
+	//{
+		mssimV = getMSSIM(img1,img3);
+
+		cout << " MSSIM: "
+			<< " R " << setiosflags(ios::fixed) << setprecision(2) << mssimV.val[2] * 100 << "%"
+			<< " G " << setiosflags(ios::fixed) << setprecision(2) << mssimV.val[1] * 100 << "%"
+			<< " B " << setiosflags(ios::fixed) << setprecision(2) << mssimV.val[0] * 100 << "%";
+	//}
 
 	cout << endl;
+	int m2=mssimV.val[2];
+
 
 	////////////////////////////////// Show Image /////////////////////////////////////////////
 	imshow( WIN_RF, img1);
-	imshow( WIN_UT, img2);
+	imshow( WIN_UT1, img2);
+	imshow( WIN_UT2, img3);
 
 	//c = cvWaitKey(delay);
 	//if (c == 27) break;
+	if (m1>m2) cout << "IMG2 é mais parecida com imagem de referencia" <<endl;
+	else cout << "IMG3 é mais parecida com imagem de referencia" <<endl;
 	  waitKey(0);
     return 0;
 }
