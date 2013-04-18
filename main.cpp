1193578bfe15dabd69a7ff3ab543355fce44b982
Jeronimo Grandi <jggrandi@gmail.com>
2013-04-18 18:04:12 -0300

    Sort utilizando STL, gerar log completo com todos algoritmos e planos em uma passada e em um arquivo (BUGADO ainda).

--- main.cpp
+++ main.cpp
@@ -25,6 +25,9 @@ vector<Mat> datasetSlices[2];
 
 FILE* inFile;
 ofstream outFile;
+vector<int> sliceAndDistance[3][3];
+vector<int> rankSumm[3];
+
 
 Scalar getPSNR ( const Mat& I1, const Mat& I2);
 Scalar getMSSIM( const Mat& I1, const Mat& I2);
@@ -42,35 +45,39 @@ void onTrackbar( int val, void* )
 	imshow( "Dataset2", datasetSlices[1][val]);
 }
 
+bool mySort (sliceRank i,sliceRank j) 
+{ 
+	return (i.value>j.value); 
+}
+bool mySort2 (sliceRank i,sliceRank j) 
+{ 
+	return (i.value<j.value); 
+}
+
 void ordenaRank(int slices, vector<sliceRank> &srr,int algorithm)
 {
-	for(int j=0; j<slices; j++) //bubble bunda pra gerar o ranking
+	if(algorithm!=3)
 	{
-		for(int i=0; i<slices-1; i++)
-		{
-			if(algorithm != 3)
-			{
-				if(srr[i+1].value > srr[i].value)
-					swap(srr[i+1], srr[i]);
-			}
-			else
-			{
-				if(srr[i+1].value < srr[i].value) //se o algoritmo é surf, a ordem de ordenação deve ser diferente
-					swap(srr[i+1], srr[i]);
-
-			}
-
-		}
+		sort(srr.begin(),srr.end(),mySort);
+	}
+	else
+	{
+		sort(srr.begin(),srr.end(),mySort2);
 	}
 }
 
-sliceRank* calculaSimilaridade(int algorithm, int slices)
+void calculaSimilaridade(int planeOrientation, int algorithm, int slices)
 {
 	int summ[10];
 
 	for(int j=0;j<10;j++)
-		summ[j]=0;
-	
+		summ[j]=-1;
+
+//	for(int i=0;i<4;i++)
+//	{
+		//sliceAndDistance.resize(4);
+		//rankSumm[i].resize(
+	//}
 	Scalar mssimV,msurfV,mpsnrV;
 	sliceRank sr_raw;
 	vector<sliceRank> sr,sr_ranked,sr_ranked2;
@@ -146,118 +153,74 @@ sliceRank* calculaSimilaridade(int algorithm, int slices)
 
 			ordenaRank(RANK_SIZE,sr_ranked2,algorithm);
 			
-			printf("%d\n",i);
-			for(int k=0; k<RANK_SIZE;k++)
-			{
-				printf("<R1> sN:%d, v:%f - <R2> sN:%d, v:%f\n",sr_ranked[k].sliceNumber,sr_ranked[k].value,sr_ranked2[k].sliceNumber,sr_ranked2[k].value);
+			//printf("%d\n",i);
+			//for(int k=0; k<RANK_SIZE;k++)
+			//{
+				//printf("<R1> sN:%d, v:%f - <R2> sN:%d, v:%f\n",sr_ranked[k].sliceNumber,sr_ranked[k].value,sr_ranked2[k].sliceNumber,sr_ranked2[k].value);
 				//outFile<<sr_ranked[k].sliceNumber<<";"<<sr_ranked2[k].sliceNumber<<endl;
-			}
-			outFile << i<<endl; 
+			//}
+			//outFile << i<<endl; 
 			for (int k=0; k<RANK_SIZE; k++)
 			{
-				int srr1=sr_ranked[k].sliceNumber-i;
 				int srr2=sr_ranked2[k].sliceNumber-i;
-				if(srr1<0) srr1=srr1*-1; 
 				if(srr2<0) srr2=srr2*-1;
-				if(k==0) outFile<<srr1<<";"<<srr2<<endl;
-//				if(srr1<=srr2)
-//				{
-//					if(sr_ranked[k].sliceNumber==i) //verifica se o PSNR encontrou o slice na mesma posiçao do dataset analizado  
-//					{
-//						summ[k]++;	//incrementa summ para exibir qntos slices foram exatamente encontrados na mesma posiçao do dataset analizado
-//						break;				
-//					}
-//				}
-//				else if(srr1>srr2)
-//				{
-					if(sr_ranked2[k].sliceNumber==i) //verifica se o SSIM encontrou o slice na mesma posiçao do dataset analizado  
-					{
-						summ[k]++;
-						break;
-					}
-//				}
+				if(k==0)
+				{
+					sliceAndDistance[planeOrientation][algorithm].push_back(i); sliceAndDistance[planeOrientation][algorithm].push_back(srr2);
+				} 
+				//outFile<<i<<";"<<srr2<<endl;
+				if(sr_ranked2[k].sliceNumber==i) //verifica se o SSIM encontrou o slice na mesma posiçao do dataset analizado  
+				{
+					if(summ[k]==-1) summ[k]*=-1;
+					else			summ[k]++;
+					break;
+				}
+
 			}
 
-			//for(int l=0; l < RANK_SIZE; l++)
-			//{
-				//cout<<"Slice:"<<sr[l].sliceNumber<<" Rank:"<<sr[l].value<<endl;
-				//outFile<<sr_ranked[l].sliceNumber<<endl;
-				//cout<<sr_ranked[l].sliceNumber<<endl;
-			//}
-			//outFile << timeAlgorithm <<endl;
 			sr_ranked2.clear();
 		}
 		else // se o algoritmo != 0 monta ranking comum
 		{	
-			printf("%d\n",i);
+			//printf("%d\n",i);
 			for (int k=0; k<RANK_SIZE; k++)
 			{
-				printf("<%d> sN:%d, v:%f\n",k,sr_ranked[k].sliceNumber,sr_ranked[k].value);
+				//printf("<%d> sN:%d, v:%f\n",k,sr_ranked[k].sliceNumber,sr_ranked[k].value);
 
 				int srr1=sr_ranked[k].sliceNumber-i;
 				if(srr1<0) srr1=srr1*-1; 
-				if(k==0) outFile<< i<<";"<<srr1<<endl;							
+				if(k==0)
+				{
+					sliceAndDistance[planeOrientation][algorithm].push_back(i); sliceAndDistance[planeOrientation][algorithm].push_back(srr1);
+				} 
+					//outFile<< i<<";"<<srr1<<endl;							
 				if(sr_ranked[k].sliceNumber==i) //verifica se encontrou o slice na mesma posiçao do dataset analizado  
 				{
-					summ[k]++;	//incrementa summ para exibir qntos slices foram exatamente encontrados na mesma posiçao do dataset analizado
+					if(summ[k]==-1) summ[k]*=-1;
+					else			summ[k]++;	//incrementa summ para exibir qntos slices foram exatamente encontrados na mesma posiçao do dataset analizado
 					break;				
 				}
 			}			
 		}
 	}
-	int sumTotal=0;
-	for(int ll=0; ll < RANK_SIZE; ll++)
-	{
-		outFile <<endl<< summ[ll];
-		cout <<endl<< summ[ll];
-		sumTotal+=summ[ll];
-	}
-	printf("\n%d\n",sumTotal);
-	outFile << endl<<sumTotal<<endl;
+	for(int i=0;i<slices;i+=2)
+		cout << sliceAndDistance[planeOrientation][algorithm][i]<<" "<< sliceAndDistance[planeOrientation][algorithm][i+1];
+	
+	for(int i=0;i<RANK_SIZE;i++)
+		rankSumm[algorithm].push_back(summ[i]);
+//	int sumTotal=0;
+//	for(int ll=0; ll < RANK_SIZE; ll++)
+//	{
+//		outFile <<endl<< summ[ll];
+//		//cout <<endl<< summ[ll];
+//		sumTotal+=summ[ll];
+//	}
+//	//printf("\n%d\n",sumTotal);
+//	outFile << endl<<sumTotal<<endl;
 
 	//return sr_ranked;
-	return NULL;
 }
-void rankBuilder(char* dataset0, char* dataset1, int width, int height,int slices, int algorithm, int planeOrientation)
-{
-	sliceRank *sr_result;
-	double timeAlgorithm;
-	//monta nome do arquivo de log
-	char nFile[200];
-	stringstream output;
-	output << dataset0<<"_"<<dataset1<<"_"<<slices<<"_"<<algorithm<<"_"<<planeOrientation;
-	string sulfix = output.str();
-	const char* ss = sulfix.c_str();
-	//strcpy(nFile,"Logs/");
-	//strcat(nFile,ss);
-	strcpy(nFile,"a");
-	strcat(nFile,".csv");
-	outFile.open(nFile, ios::out);
-	if(outFile.fail())
-	{
-		printf( "Could not open output file %s.\n", nFile );
-		exit(-1);
-	}
-	//escreve cabeçalho do arquivo de log
-	outFile<<"Dataset1;"<<"Dataset2;"<<"width;"<<"height;"<<"slices;"<<"algorithm;"<<"planeOrientation"<<endl;
-	outFile<<dataset0<<";"<<dataset1<<";"<<width<<";"<<height<<";"<<slices<<";"<<algorithm<<";"<<planeOrientation<<endl;
 
-	double timeTotal = (double)getTickCount();		
-	if(algorithm==4)//se algorithm == 3 os datasets são processados utilizando-se todas as técnicas 
-	{
-		for(int j=0, algorithm=0;j<4;j++,algorithm++)
-		{
-			sr_result=calculaSimilaridade(j,slices);
-		}
-	}
-	else
-	{
-		sr_result=calculaSimilaridade(algorithm,slices);
-	}
-
-	timeTotal = ((double)getTickCount() - timeTotal)/getTickFrequency();
-	outFile << endl<<"Total time: " << timeTotal <<endl;
-}
 
 
 int loadDatasets(char** l_datasets,short unsigned int l_width, short unsigned int l_height, short unsigned int l_sliceRange[])
@@ -337,15 +300,15 @@ int changePlane(short unsigned int c_width, short unsigned int c_height, short u
 					}
 					else if(c_planeOrientation==1)
 					{
-						if(k==1 && i>=5) //offset para a combinacao tardia x venosa e arterial
+						if(k==1 && i>=8) //offset para a combinacao tardia x venosa e arterial
 							datasetNewPlane[k][i-2][j][l] = datasetRaw[k][l][i][j]; // XZ plane
 						else if(k==0)
 							datasetNewPlane[k][i][j][l] = datasetRaw[k][l][i][j]; // XZ plane
 					}
 					else if(c_planeOrientation==2)
 					{
-						if(k==0 && i>=6) //offset para a combinacao tardia x venosa e arterial
-							datasetNewPlane[k][i-3][j][l] = datasetRaw[k][l][j][i]; // YZ plane
+						if(k==0 && i>=8) //offset para a combinacao tardia x venosa e arterial
+							datasetNewPlane[k][i-2][j][l] = datasetRaw[k][l][j][i]; // YZ plane
 						else if(k==1)
 							datasetNewPlane[k][i][j][l] = datasetRaw[k][l][j][i]; // YZ plane
 					}
@@ -397,7 +360,7 @@ void splitDatasets(short unsigned int s_width,short unsigned int s_height,short 
 			}
 		}
 	}
-	printf("aqui");
+
 	for( int k = 0; k < QNT_DATASETS; k++ )
 	{
 		for( int i = 0; i < s_slices; i++ )
@@ -408,8 +371,96 @@ void splitDatasets(short unsigned int s_width,short unsigned int s_height,short 
 			datasetSlices[k].push_back(plane);
 		}
 	}
-	printf("Split datasets done!\n");
 }
+
+
+int rankBuilder(char* dataset[], int width, int height,short unsigned int sliceRange[], int algorithm, int planeOrientation)
+{
+
+	int slices=sliceRange[1]-sliceRange[0];
+
+	//monta nome do arquivo de log
+	char nFile[200];
+	stringstream output;
+	output << dataset[0]<<"_"<<dataset[1]<<"_"<<slices<<"_combined";
+	string sulfix = output.str();
+	const char* ss = sulfix.c_str();
+	//strcpy(nFile,"Logs/");
+	//strcat(nFile,ss);
+	strcpy(nFile,"a");
+	strcat(nFile,".csv");
+	outFile.open(nFile, ios::out);
+	if(outFile.fail())
+	{
+		printf( "Could not open output file %s.\n", nFile );
+		exit(-1);
+	}
+	//escreve cabeçalho do arquivo de log
+	//outFile<<"Dataset1;"<<"Dataset2;"<<"width;"<<"height;"<<"slices;"<<"algorithm;"<<"planeOrientation"<<endl;
+	outFile<<width<<"x"<<height<<"x"<<slices<<";["<<sliceRange[0]<<"-"<<sliceRange[1]<<"]; Plane:"<<planeOrientation<<endl;
+	outFile<<"Plane:0;;;;;;;;;Plane:1;;;;;;;;;Plane:2"<<endl;
+	outFile<<"PSNR+SSIM;;;PSNR;;;SSIM;;;PSNR+SSIM;;;PSNR;;;SSIM;;;PSNR+SSIM;;;PSNR;;;SSIM"<<endl;
+	double timeTotal = (double)getTickCount();		
+
+	printf("Loading datasets...  ");
+	if(loadDatasets(dataset,width,height,sliceRange)==-1)
+		return -1;		
+	printf("DONE!\n");
+
+	if(algorithm==4)//se algorithm == 3 os datasets são processados utilizando-se todas as técnicas 
+	{
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				printf("Changing plane...  ");
+				if(changePlane(width,height,sliceRange,j)==-1)
+					return -1;
+				printf("DONE!\n");
+
+				printf("Spliting datasets...  ");
+				splitDatasets(width,height,sliceRange,j);
+				printf("DONE!\n");	
+				calculaSimilaridade(j,i,slices);		
+			}
+		}
+	}
+	else
+	{
+		printf("Changing plane...  ");
+		if(changePlane(width,height,sliceRange,planeOrientation)==-1)
+			return -1;
+		printf("DONE!\n");
+
+		printf("Spliting datasets...  ");
+		splitDatasets(width,height,sliceRange,planeOrientation);
+		printf("DONE!\n");		
+	
+		calculaSimilaridade(planeOrientation,algorithm,slices);
+	}
+
+	timeTotal = ((double)getTickCount() - timeTotal)/getTickFrequency();
+	
+	//build the csv formated
+
+	for(int i=0;i<slices;i+=2)
+	{
+		for(int j=0;j<3;j++)
+		{
+			for(int k=0;k<3;k++)
+			{
+				cout<<sliceAndDistance[k][j][i]<<" "<<sliceAndDistance[k][j][i+1]<<endl;
+				outFile<<sliceAndDistance[k][j][i]<<";"<<sliceAndDistance[k][j][i+1]<<";;";
+			}
+		}
+		outFile<<endl;
+	}
+
+
+	outFile << endl<<"Total time: " << timeTotal <<endl;
+	return 0;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -427,51 +478,18 @@ int main(int argc, char *argv[])
 
 	short unsigned int slices = sliceRange[1]-sliceRange[0];
 
-	if(loadDatasets(dataset,width,height,sliceRange)==-1)
-		return -1;		
-	printf("Loading datasets done!\n");
+	printf("Building ranking...  ");	
+	if(rankBuilder(dataset,width,height,sliceRange,algorithm,planeOrientation)==-1)
+		return -1;
 
-	switch(planeOrientation)
-	{
-		case 0:
-		{
-			if(changePlane(width,height,sliceRange,planeOrientation)==-1)
-				return -1;
-			printf("Change plane done!\n");			
-			splitDatasets(width,height,sliceRange,planeOrientation);
-			break;
+	printf("DONE!\n");
 
-		}
-		case 1:
-		{
-			if(changePlane(width,height,sliceRange,planeOrientation)==-1)
-				return -1;
-			printf("Change plane done!\n");
-			splitDatasets(width,height,sliceRange,planeOrientation);
-			break;
-		}
-		case 2:
-		{
-			if(changePlane(width,height,sliceRange,planeOrientation)==-1)
-				return -1;
-			printf("Change plane done!\n");
-			splitDatasets(width,height,sliceRange,planeOrientation);
-			break;			
-		}
-	}
-
-
-	
 	namedWindow("Trackbar",0);
 	if(planeOrientation==0)
 		createTrackbar("TB","Trackbar",0,slices-1,onTrackbar);
 	else
 		createTrackbar("TB","Trackbar",0,slices-1,onTrackbar);
 	onTrackbar(0,0);
-
-	rankBuilder(dataset[0],dataset[1],width,height,slices,algorithm,planeOrientation);
-
-//	cout << "Slice mais parecido:"<<out[1] << " "<< setprecision(3)<<out[0]*100<<"%"<<endl;
 
 	waitKey(0);
 	destroyAllWindows();
